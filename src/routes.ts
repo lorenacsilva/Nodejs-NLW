43f3735a009d6b6fa095be7ff0b9709d4c471516
Lorena C Silva <loricristinesilva@gmail.com>
2021-12-29 17:48:54 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import {Router} from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
+//Geralmente o nome do recurso nas API's é definido no plural
 router.post("/users", createUserController.handle );
+router.post("/tags", createTagController.handle); 
 
-export {router};
+export { router };
